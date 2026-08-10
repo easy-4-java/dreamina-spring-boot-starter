4b1e93bf0da87cf4e8fc4179e117d9944cf03516
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 16:26:36 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/test/java/io/github/easy4j/dreamina/spring/boot/DreaminaAutoConfigurationTest.java
+++ src/test/java/io/github/easy4j/dreamina/spring/boot/DreaminaAutoConfigurationTest.java
@@ -12,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 /**
  * Starter 自动配置基础测试。
  *
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  * @since 1.0.0
  */
 @SpringBootTest(
